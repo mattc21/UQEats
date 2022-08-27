0f18d3137c3c25f257cf54cee31464eeb0600a70
Ryan McNeilly <ryanmcn@live.com.au>
2022-08-27 11:50:24 +1000

    Stopped duplicates from appearing in homescreen

--- UQEats/screens/HomeScreen.js
+++ UQEats/screens/HomeScreen.js
@@ -1,36 +1,38 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { SafeAreaView, ScrollView, View, Image, StyleSheet } from "react-native";
 import RestaurantItem from "../components/RestaurentItem";
 
 const HomeScreen = () => {
     return (
-        <View style = {homeScreenStyles.background} cont>
-            <ScrollView style = {homeScreenStyles.background}showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1, paddingVertical: 50 }}>
-                <Image
-                    source={
-                        require("../assets/UQEats(1).png")
-                    }
-                    style = {homeScreenStyles.logo}
-                />
-                <RestaurantItem />
-                <RestaurantItem />
-                <RestaurantItem />
-            </ScrollView>
-        </View>
+        <NavigationContainer>
+            <View style = {homeScreenStyles.background} cont>
+                <ScrollView style = {homeScreenStyles.background}showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1, paddingVertical: 50 }}>
+                    <Image
+                        source={
+                            require("../assets/UQEats(1).png")
+                        }
+                        style = {homeScreenStyles.logo}
+                    />
+                    <RestaurantItem />
+                </ScrollView>
+            </View>
             
+        </NavigationContainer>
+        
 
     )
 }
 
 const homeScreenStyles = StyleSheet.create({
     logo: {
-        width: "80%",
-        height: "30%",
+        width: "90%",
+        height: "5%",
         margin: 10,
-        left: 25,
+        left: 8,
     },
     background: {
-        backgroundColor: "#eee",
+        backgroundColor: "#CBC3E3",
         flexGrow: 1,
         height: "100%",
         width: "100%",
