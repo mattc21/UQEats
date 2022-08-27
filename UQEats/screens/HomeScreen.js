7590543872be1031d4ef77148fb484de69e9ace3
Ryan McNeilly <ryanmcn@live.com.au>
2022-08-27 10:53:02 +1000

    Added RestruntItems with ImageURL

--- UQEats/screens/HomeScreen.js
+++ UQEats/screens/HomeScreen.js
@@ -1,18 +1,41 @@
 import React from "react";
-import { SafeAreaView, ScrollView, View } from "react-native";
+import { SafeAreaView, ScrollView, View, Image, StyleSheet } from "react-native";
 import RestaurantItem from "../components/RestaurentItem";
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView >
-            <ScrollView showsVerticalScrollIndicator={false}>
+        <View style = {homeScreenStyles.background} cont>
+            <ScrollView style = {homeScreenStyles.background}showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1, paddingVertical: 50 }}>
+                <Image
+                    source={
+                        require("../assets/UQEats(1).png")
+                    }
+                    style = {homeScreenStyles.logo}
+                />
                 <RestaurantItem />
                 <RestaurantItem />
                 <RestaurantItem />
             </ScrollView>
-        </SafeAreaView>
+        </View>
+            
 
     )
 }
+
+const homeScreenStyles = StyleSheet.create({
+    logo: {
+        width: "80%",
+        height: "30%",
+        margin: 10,
+        left: 25,
+    },
+    background: {
+        backgroundColor: "#eee",
+        flexGrow: 1,
+        height: "100%",
+        width: "100%",
+        top: 0
+    }
+});
 
 export default HomeScreen;
